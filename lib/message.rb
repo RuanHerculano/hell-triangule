4dee06e6fb151f2e6e78032838712172f7eca2e8
ruanHerculano <ruannawe95@gmail.com>
2018-01-14 15:02:55 -0200

    refatorando mensagens

--- lib/message.rb
+++ lib/message.rb
@@ -1,22 +1,24 @@
 class Message
   def self.result(numbers)
+    sum, message = build_array_message(numbers)
+
+    message.concat(" = #{sum}")
+    message
+  end
+
+  def self.build_array_message(numbers)
     amount_numbers = numbers.count - 1
-    sum = 0
-    index = 0
     message = ''
+    index = 0
+    sum = 0
 
     numbers.each do |number|
       message.concat(number.to_s)
       sum += number
-
-      if index != amount_numbers
-        message.concat(' + ')
-      end
-
+      message.concat(' + ') if index != amount_numbers
       index += 1
     end
 
-    message.concat(" = #{sum}")
-    message
+    return sum, message
   end
 end
